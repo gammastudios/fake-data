cb722144d11735ca0d88601b118ce550efce16ab
Chris Ottinger <chris.ottinger@gammadata.io>
2024-03-04 21:15:57 +1100

    style(metadata-cache): formatting fix ups
    #8

--- tests/metadata/test_metadata_cache.py
+++ tests/metadata/test_metadata_cache.py
@@ -4,6 +4,7 @@ from fake_data.metadata.metadata_cache import MetadataCache
 import os
 import pytest
 
+
 @pytest.fixture(scope="session")
 def tmp_cache_dir():
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -11,17 +12,17 @@ def tmp_cache_dir():
 
 
 class TestMetadataCache:
-
     def test_init_default(self):
+        # not the best test as the file could be created outside of the test case
         expected_cache_dir = "./.fake_data_cache"
-        mdc = MetadataCache()
+        _ = MetadataCache()
 
         # Check if the file "fake_data.db" exists the default metadata cache directory
         assert os.path.isfile(f"{expected_cache_dir}/fake_data.db")
 
     def test_init_with_cache_dir(self, tmp_cache_dir):
         # Perform testing with the temporary cache directory
-        mdc = MetadataCache(metadata_cache_dir=tmp_cache_dir)
+        _ = MetadataCache(metadata_cache_dir=tmp_cache_dir)
 
         # Check if the file "fake_data.db" exists in the cache_dir directory
         assert os.path.isfile(os.path.join(tmp_cache_dir, "fake_data.db"))
@@ -30,8 +31,8 @@ class TestMetadataCache:
         mdc = MetadataCache()
         table_name = "test_table"
         columns = [
-            {'name': "id", 'type': "int"},
-            {'name': "name", 'type': "string"},
+            {"name": "id", "type": "int"},
+            {"name": "name", "type": "string"},
         ]
         expected = "CREATE TABLE test_table (id int, name string)"
         assert mdc._create_table_sttmt(table_name, columns) == expected
