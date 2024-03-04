b24b5ef73af825a91aeb6be606a7d298d12a7d2f
Chris Ottinger <chris.ottinger@gammadata.io>
2024-03-04 17:32:27 +1100

    style(cicd): tmp setting to ignore linting errors
    #8

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,9 @@
-
 from typer.testing import CliRunner
 
 from fake_data.generate_fake_data import app
 
 runner = CliRunner()
+
 
 def test_fake_data_cli_version():
     result = runner.invoke(app, ["version"])
