bde2a9226567f2b296ba7c03522292e84dd82413
Chris Ottinger <chris.ottinger@gammadata.io>
2024-03-04 21:10:22 +1100

    feat(metadata-cache): Initial metadata cache scaffolding
    #8

---
+++ fake_data/metadata/metadata_cache.py
@@ -0,0 +1,45 @@
+import duckdb
+import os
+from typing import List
+
+class MetadataCache:
+    """
+    Metadata cache for the fake data generator.
+    """
+
+    def __init__(
+            self,
+            metadata_cache_dir:str='./.fake_data_cache'
+        ) -> None:
+
+        # create the cache dir if it does not exist
+        self.metadata_cache_dir = metadata_cache_dir
+        os.makedirs(self.metadata_cache_dir, exist_ok=True)
+
+        # Connect to the database file
+        self.db = duckdb.connect(database=os.path.join(self.metadata_cache_dir, 'fake_data.db'))
+
+        # TODO load any existing metadata from the cache database
+        self.tables = []
+
+
+    def _create_table_sttmt(self, table_name, columns: List[dict]) -> str:
+        """
+        Assemble create table DDL statement.
+        
+        Args:
+            table_name (str): name of the table.
+            columns (List[dict]): columns of the table.  Each column is a dictionary at least with the following keys:
+                - name: name of the column.
+                - type: type of the column.
+
+        Returns:
+            (str): DDL string for the create table statement.
+        """
+        sttm = ""
+        try:
+            cols_str = ', '.join(f"{c['name']} {c['type']}" for c in columns)
+            sttmt = f"CREATE TABLE { table_name } ({ cols_str })"
+        except KeyError as e:
+            raise ValueError(f"Column definition missing key: {e}")
+        return sttmt
---
+++ tests/metadata/test_metadata_cache.py
@@ -0,0 +1,37 @@
+import tempfile
+
+from fake_data.metadata.metadata_cache import MetadataCache
+import os
+import pytest
+
+@pytest.fixture(scope="session")
+def tmp_cache_dir():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        yield temp_dir
+
+
+class TestMetadataCache:
+
+    def test_init_default(self):
+        expected_cache_dir = "./.fake_data_cache"
+        mdc = MetadataCache()
+
+        # Check if the file "fake_data.db" exists the default metadata cache directory
+        assert os.path.isfile(f"{expected_cache_dir}/fake_data.db")
+
+    def test_init_with_cache_dir(self, tmp_cache_dir):
+        # Perform testing with the temporary cache directory
+        mdc = MetadataCache(metadata_cache_dir=tmp_cache_dir)
+
+        # Check if the file "fake_data.db" exists in the cache_dir directory
+        assert os.path.isfile(os.path.join(tmp_cache_dir, "fake_data.db"))
+
+    def test_create_table_sttmt(self):
+        mdc = MetadataCache()
+        table_name = "test_table"
+        columns = [
+            {'name': "id", 'type': "int"},
+            {'name': "name", 'type': "string"},
+        ]
+        expected = "CREATE TABLE test_table (id int, name string)"
+        assert mdc._create_table_sttmt(table_name, columns) == expected
