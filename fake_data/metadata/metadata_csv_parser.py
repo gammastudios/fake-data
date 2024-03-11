1273a521011843662d953aa1c8c9802e9cc73086
tomklimovskigamma <tom.klimovski@gammadata.io>
2024-03-11 16:44:09 +1100

    refactor(app): removed pandas, and also the header-list

--- fake_data/metadata/metadata_csv_parser.py
+++ fake_data/metadata/metadata_csv_parser.py
@@ -1,10 +1,8 @@
-import pandas as pd
-from typing import List
-
+import duckdb
 
 class MetadataCsvParser:
     """
-    Parser for schema definitions defined in csv files.
+    Parser for schema definitions defined in csv files using duckdb.
 
     A single entity/dataset/table is expected to be defined in a single csv file.
 
@@ -13,7 +11,7 @@ class MetadataCsvParser:
     - data_type
     - key_type: PK for primary key, FK for foreign key, and blank for regular columns.
     - reference_entity: name of the entity to which the foreign key refers.  Blank for regular columns.
-    - reference_attribute: name of the attribute in the refernece_entity which contains the foreign key.
+    - reference_attribute: name of the attribute in the reference_entity which contains the foreign key.
         Blank for regular columns.
     - relationship: cardinality of the PK-FK relationships.  Blank for regular columns.
 
@@ -23,26 +21,25 @@ class MetadataCsvParser:
     def __init__(self):
         pass
 
-    def read_csv(self, csv_file: str, header: int = 0) -> List[dict]:
+    def read_csv(self, csv_file: str, header: bool = True) -> list:
         """
-        Read a csv file and return a pandas DataFrame.
+        Read a csv file and return a list of dictionaries using duckdb.
 
         Args:
             csv_file (str): path to the csv file.
-            header (int): row number to use as the column names.  Defaults to 0.
+            header (bool): whether the csv file contains a header row. Defaults to True.
 
         Returns:
-            pd.DataFrame: the csv file as a pandas DataFrame.
+            list: the csv file as a list of dictionaries.
         """
-        columns = [
-            "attribute_name",
-            "data_type",
-            "key_type",
-            "reference_entity",
-            "reference_attribute",
-            "relationship",
-        ]
-        metadata_df = pd.read_csv(csv_file, names=columns, header=header)
-        metadata_array = metadata_df.to_dict(orient="records")
+        query = f"SELECT * FROM read_csv_auto('{csv_file}', HEADER={header})"
+        result = duckdb.query(query).fetchall()
+        columns = duckdb.query(query).description
+
+        # Extracting column names
+        column_names = [col[0] for col in columns]
+
+        # Converting list of tuples to list of dictionaries
+        metadata_array = [dict(zip(column_names, row)) for row in result]
 
         return metadata_array
